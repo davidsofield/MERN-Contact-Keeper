80a5b6bf59b01ab4dd407fe2e88507f16c00601a
David Sofield <davidsofield@gmail.com>
2019-07-24 07:16:06 -0400

    Add Context

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 
-function App() {
+import ContactState from './context/contact/ContactState'
+
+const App = () => {
 	return (
-		<Router>
-			<>
-				<Navbar />
-				<div className="container">
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/about" component={About} />
-					</Switch>
-				</div>
-			</>
-		</Router>
+		<ContactState>
+			<Router>
+				<>
+					<Navbar />
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/about" component={About} />
+						</Switch>
+					</div>
+				</>
+			</Router>
+		</ContactState>
 	)
 }
 
